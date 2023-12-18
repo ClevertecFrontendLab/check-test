784fab2751a29b76074f3d4cb709ee36c2e46932
mitrofazxc <mitrofanzxc@gmail.com>
2023-12-18 18:40:32 +0300

    refactor(*): dependency update

--- index.js
+++ index.js
@@ -4,102 +4,107 @@ const axios = require('axios');
 const fs = require('fs');
 const FormData = require('form-data');
 
+const API_URL = 'https://training.clevertec.ru';
+
 const main = async () => {
-    try {
-        const owner = core.getInput('owner', { required: true });
-        const repo = core.getInput('repo', { required: true });
-        const pull_number = core.getInput('pull_number', { required: true });
-        const token = core.getInput('token', { required: true });
-        const base_url = core.getInput('host', { required: false }) || 'https://training.cleverland.by';
-        const path_to_tests_report = 'cypress/report/report.json';
-        const path_to_test_file_name = 'cypress/e2e';
-        const minimum_required_result = 97;
-        const minimum_result_to_send_screenshots = 87;
-        let tests_result_message = '';
-        let pass_percent_tests = 0;
+  try {
+    const owner = core.getInput('owner', { required: true });
+    const repo = core.getInput('repo', { required: true });
+    const pull_number = core.getInput('pull_number', { required: true });
+    const token = core.getInput('token', { required: true });
+    const base_url = core.getInput('host', { required: false }) || API_URL;
+    const path_to_tests_report = 'cypress/report/report.json';
+    const path_to_test_file_name = 'cypress/e2e';
+    const minimum_required_result = 97;
+    const minimum_result_to_send_screenshots = 87;
+    let tests_result_message = '';
+    let pass_percent_tests = 0;
 
-        const octokit = new github.getOctokit(token);
+    const octokit = new github.getOctokit(token);
 
-        fs.readFile(path_to_tests_report, 'utf8', (err, data) => {
-            const { stats: { tests, failures, passPercent } } = JSON.parse(data);
-            pass_percent_tests = passPercent;
+    fs.readFile(path_to_tests_report, 'utf8', (err, data) => {
+      const {
+        stats: { tests, failures, passPercent },
+      } = JSON.parse(data);
+      pass_percent_tests = passPercent;
 
-            tests_result_message = '#  Результаты тестов' + '\n' + `Процент пройденных тестов: ${Math.trunc(passPercent)}%.` + '\n' + `Общее количество тестов: ${tests}.` + '\n' + `Количество непройденных тестов: ${failures}.` + '\n';
+      tests_result_message =
+        '#  Результаты тестов' +
+        '\n' +
+        `Процент пройденных тестов: ${Math.trunc(passPercent)}%.` +
+        '\n' +
+        `Общее количество тестов: ${tests}.` +
+        '\n' +
+        `Количество непройденных тестов: ${failures}.` +
+        '\n';
+    });
+
+    const { data: pull_request_info } = await octokit.rest.pulls.get({
+      owner,
+      repo,
+      pull_number,
+    });
+
+    const test_file_name = fs.readdirSync(path_to_test_file_name)[0];
+    const path_to_tests_screenshots = `cypress/report/screenshots/${test_file_name}`;
+
+    const formData = new FormData();
+    formData.append('github', pull_request_info.user.login);
+
+    fs.readdirSync(path_to_tests_screenshots).forEach((screenshot) => {
+      formData.append('files', fs.createReadStream(`${path_to_tests_screenshots}/${screenshot}`));
+    });
+
+    const screenshots_links_request_config = {
+      method: 'post',
+      url: `${base_url}/pull-request/save-images`,
+      headers: {
+        ...formData.getHeaders(),
+      },
+      data: formData,
+    };
+
+    const { data: screenshots } = await axios(screenshots_links_request_config);
+
+    const createTestsResultMessage = () => {
+      if (pass_percent_tests >= minimum_result_to_send_screenshots) {
+        screenshots.forEach(({ name, url }) => {
+          url = url.replace(/\s+/g, '%20');
+          tests_result_message +=
+            '***' + '\n' + `**${name}**` + '\n' + `![Скриншот автотестов](https://static.clevertec.ru${url})` + '\n';
         });
+      }
 
-        const { data: pull_request_info } = await octokit.rest.pulls.get({
-            owner,
-            repo,
-            pull_number,
-        });
+      return tests_result_message;
+    };
 
-        const test_file_name = fs.readdirSync(path_to_test_file_name)[0];
-        const path_to_tests_screenshots = `cypress/report/screenshots/${test_file_name}`;
+    await octokit.rest.issues.createComment({
+      owner,
+      repo,
+      issue_number: pull_number,
+      body: createTestsResultMessage(),
+    });
 
-        const formData = new FormData();
-        formData.append('github', pull_request_info.user.login);
-        
-        fs.readdirSync(path_to_tests_screenshots).forEach(screenshot => {
-            formData.append('files', fs.createReadStream(`${path_to_tests_screenshots}/${screenshot}`));
-        });
+    const tests_result_request_config = {
+      method: 'post',
+      url: `${base_url}/pull-request/opened`,
+      headers: {
+        'Content-Type': 'application/json;charset=utf-8',
+      },
+      data: {
+        link: pull_request_info.html_url,
+        github: pull_request_info.user.login,
+        isTestsSuccess: pass_percent_tests >= minimum_required_result,
+        pullNumber: pull_number,
+      },
+    };
 
-        const screenshots_links_request_config = {
-            method: 'post',
-            url: `${base_url}/pull-request/save-images`,
-            headers: { 
-                ...formData.getHeaders()
-            },
-            data: formData
-        };
-
-        const { data: screenshots } = await axios(screenshots_links_request_config);
-
-        const createTestsResultMessage = () => {
-            if (pass_percent_tests >= minimum_result_to_send_screenshots) {
-                screenshots.forEach(({ name, url }) => {
-                    console.log('name', name);
-                    console.log('url', url);
-                    url = url.replace(/\s+/g,'%20');
-                    console.log(`***https://training.clevertec.ru${url}***`);
-                    console.log(`***ttps://training.clevertec.ru${url}***`);
-                    console.log(`**![Скриншот автотестов](https://training.clevertec.ru${url})***`);
-                    console.log(`![Скриншот автотестов](https://training.clevertec.ru${url})`);
-                    tests_result_message += '***' + '\n' + `**${name}**` + '\n' + `![Скриншот автотестов](https://training.clevertec.ru${url})` + '\n';
-                    console.log(`111***${tests_result_message}***111`);
-                });
-            }
-           
-            return tests_result_message;
-        };
-
-        await octokit.rest.issues.createComment({
-            owner,
-            repo,
-            issue_number: pull_number,
-            body: createTestsResultMessage(),
-        });
-
-        const tests_result_request_config = {
-            method: 'post',
-            url: `${base_url}/pull-request/opened`,
-            headers: {
-                'Content-Type': 'application/json;charset=utf-8'
-            },
-            data : { 
-                link: pull_request_info.html_url, 
-                github: pull_request_info.user.login,
-                isTestsSuccess: pass_percent_tests >= minimum_required_result,
-                pullNumber: pull_number
-            },
-        };
-
-        await axios(tests_result_request_config);
-
-    } catch (error) {
-        console.log(error);
-        core.setFailed(error.message);
-    }
-}
+    await axios(tests_result_request_config);
+  } catch (error) {
+    console.log(error);
+    core.setFailed(error.message);
+  }
+};
 
 // Call the main function to run the action
 main();
